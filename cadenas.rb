6f881943e4f92be5313e4caef18ea480120fd08e
SamRev2 <127128690+SamRev2@users.noreply.github.com>
2024-08-30 03:51:25 +0000

    crear archivo string

---
+++ cadenas.rb
@@ -0,0 +1,12 @@
+class cadenas
+    def cadena_uno(cadena)
+        cadena = "Hola mundo"
+    end
+  
+    def cadena_dos(cadena)
+        cadena = "Mi nombre es Sam"
+    end
+  
+    def cadena_tres(cadena)
+        cadena = "Mucho gusto"
+    end
