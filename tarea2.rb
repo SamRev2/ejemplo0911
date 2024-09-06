6ca4bbaadc8b4d40add05d4f9a4c15e44d96ed89
SamRev2 <127128690+SamRev2@users.noreply.github.com>
2024-09-06 01:02:34 +0000

    Se agrega tarea 2

--- tarea2.rb
+++ tarea2.rb
@@ -5,19 +5,19 @@ class ContadordePalabras
   
     def contar_palabras
       palabras = @cadena.downcase.split  
-      frecuencia = Hash.new(0)            
+      contador = Hash.new(0)            
   
       palabras.each do |palabra|
-        frecuencia[palabra] += 1
+        contador[palabra] += 1
       end
   
-      frecuencia.each do |palabra, cuenta|
-        puts "#{palabra} #{cuenta}"
+      contador.each do |palabra, numero|
+        puts "La palabra #{palabra} se repitió un total de #{numero} veces"
       end
     end
 end
   
-texto = "Casa CARRO bicicleta carro"
-instancia = ContadordePalabras.new(texto)
+cadena = "Casa CARRO bicicleta carro"
+instancia = ContadordePalabras.new(cadena)
 instancia.contar_palabras
 
